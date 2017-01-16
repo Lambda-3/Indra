11aefcdc487278cb92f26f559eeb1eaae1b067ef
Leonardo Souza <lsouza@amtera.com.br>
2017-01-16 15:44:36 +0100

    Issue#4 Licensing model in place. GPLv2 enforced because one of the dependencies of the project. We plan to drop this license very soon!

--- indra-core/src/main/java/org/lambda3/indra/core/Params.java
+++ indra-core/src/main/java/org/lambda3/indra/core/Params.java
@@ -1,5 +1,27 @@
 package org.lambda3.indra.core;
 
+/*-
+ * ==========================License-Start=============================
+ * Indra Core Module
+ * --------------------------------------------------------------------
+ * Copyright (C) 2016 - 2017 Lambda^3
+ * --------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * ==========================License-End===============================
+ */
+
 import org.lambda3.indra.common.client.Language;
 import org.lambda3.indra.common.client.Model;
 import org.lambda3.indra.common.client.ScoreFunction;
