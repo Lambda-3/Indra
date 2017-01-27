50e6a68b31e00f6cbe8f87c956571a213cbaa3ca
Leonardo Souza <lsouza@amtera.com.br>
2017-01-27 22:55:22 +0100

    Updating docker build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 # Developer helper
 
-mvn clean package && \
-docker build --rm=false --build-arg INDRA_VERSION=1.0.0-SNAPSHOT -t indra:1.0-rc .
+mvn -q clean package && \
+docker build --rm=false --build-arg INDRA_VERSION=1.0.0-SNAPSHOT -t lambdacube/indra:1.0-rc1 .
