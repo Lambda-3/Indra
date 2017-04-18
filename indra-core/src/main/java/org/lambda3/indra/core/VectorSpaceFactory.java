b960fe9c32434ee5ac382d03640dbd78c277c2b2
Juliano Efson Sales <julianoens@gmail.com>
2017-04-19 01:35:40 +0200

    version number and available resources via get

--- indra-core/src/main/java/org/lambda3/indra/core/VectorSpaceFactory.java
+++ indra-core/src/main/java/org/lambda3/indra/core/VectorSpaceFactory.java
@@ -28,10 +28,15 @@ package org.lambda3.indra.core;
 
 import org.lambda3.indra.core.composition.VectorComposerFactory;
 
+import java.util.Collection;
+
 public abstract class VectorSpaceFactory<T extends VectorSpace> extends IndraCachedFactory<T> {
     protected VectorComposerFactory vectorComposerFactory;
 
     public VectorSpaceFactory() {
         this.vectorComposerFactory = new VectorComposerFactory();
     }
+
+    public abstract Collection<String> getAvailableModels();
+
 }
